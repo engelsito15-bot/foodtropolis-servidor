fa40d149a3f86dd5af677ed68bc8470d7a6cf98e
Joan Cruz <joancruz0502@hotmail.com>
2026-03-13 12:51:07 -0400

    Agregando SSL para la base de datos

--- index.js
+++ index.js
@@ -43,10 +43,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  // ESTA ES LA LÍNEA MÁGICA:
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
